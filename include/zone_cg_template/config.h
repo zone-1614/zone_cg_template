aa26cff2ff4fcd1acd384f13adb3d2bac8ac6fdd
Zone <2493920954@qq.com>
2023-03-28 10:17:21 +0800

    current path

--- include/zone_cg_template/config.h
+++ include/zone_cg_template/config.h
@@ -1,2 +1,6 @@
 #pragma once
-#define model_path "D:/code/cpp/cg/zone_cg_template/model/"
+// #define model_path "D:/code/cpp/cg/zone_cg_template/model/"
+#include <string>
+#include <filesystem>
+static std::string curr = std::filesystem::current_path().string();
+static std::string model_path = "D:/code/cpp/cg/zone_cg_template/model/";
